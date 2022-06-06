7ec3a7fdd870837db4e6362125fa64442a05a166
IngaElsta <inga.elsta@gmail.com>
2022-06-06 14:19:19 +0300

    Replacing direct call to FavoriteLocationService with calling its API

--- src/main/java/com/github/ingaelsta/weatherinfo/weather/service/WeatherCacheSchedulingService.java
+++ src/main/java/com/github/ingaelsta/weatherinfo/weather/service/WeatherCacheSchedulingService.java
@@ -2,35 +2,55 @@ package com.github.ingaelsta.weatherinfo.weather.service;
 
 import com.github.ingaelsta.weatherinfo.commons.model.Location;
 import com.github.ingaelsta.weatherinfo.favorites.entity.FavoriteLocation;
-import com.github.ingaelsta.weatherinfo.favorites.service.FavoriteLocationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
+@Slf4j
 @Service
 public class WeatherCacheSchedulingService {
 
     private final WeatherService weatherService;
+    private final WebClient webClient;
 
-    //todo: might be better to call favorite location controller API instead of service and make those functionalities independent
-    private final FavoriteLocationService favoriteLocationService;
+    private String favoriteURI = "http://localhost:8080/api/v1/weather-info/favorites";
 
 
-    public WeatherCacheSchedulingService(WeatherService weatherService, FavoriteLocationService favoriteLocationService) {
+    public WeatherCacheSchedulingService(WeatherService weatherService) {
         this.weatherService = weatherService;
-        this.favoriteLocationService = favoriteLocationService;
+        this.webClient = WebClient.create();
+    }
+
+    public void setFavoriteURI(String favoriteURI) {
+        this.favoriteURI = favoriteURI;
     }
 
     @Scheduled(cron = "${cron.fetch}")
     public void cacheWeatherForFavoriteLocations () {
-        List<FavoriteLocation> allFavoriteLocations =  favoriteLocationService.getAllFavorites();
+        List<FavoriteLocation> allFavoriteLocations = retrieveFavorites();
+        System.out.println(allFavoriteLocations);
         allFavoriteLocations
                 .forEach(favorite -> {
                     Location location = new Location(favorite.getLatitude(), favorite.getLongitude());
                     weatherService.evictWeatherCacheValue(location);
                     weatherService.getWeather(location);
                 });
+    }
+
+    private List<FavoriteLocation> retrieveFavorites() {
+        List<FavoriteLocation> favoriteList = webClient.get()
+                .uri(favoriteURI)
+                .retrieve()
+                .bodyToFlux(FavoriteLocation.class)
+                .collectList()
+                .block(Duration.of(3000, ChronoUnit.MILLIS));
+        System.out.println(favoriteList);
+        return favoriteList;
     }
 
     @Scheduled(cron = "${cron.evict}")
--- src/test/java/com/github/ingaelsta/weatherinfo/weather/service/WeatherCacheSchedulingServiceTest.java
+++ src/test/java/com/github/ingaelsta/weatherinfo/weather/service/WeatherCacheSchedulingServiceTest.java
@@ -1,16 +1,23 @@
 package com.github.ingaelsta.weatherinfo.weather.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.ingaelsta.weatherinfo.commons.Conversion;
 import com.github.ingaelsta.weatherinfo.commons.model.Location;
 import com.github.ingaelsta.weatherinfo.favorites.entity.FavoriteLocation;
-import com.github.ingaelsta.weatherinfo.favorites.service.FavoriteLocationService;
 import com.github.ingaelsta.weatherinfo.weather.model.Alert;
 import com.github.ingaelsta.weatherinfo.weather.model.Temperature;
 import com.github.ingaelsta.weatherinfo.weather.model.WeatherConditions;
 import com.github.ingaelsta.weatherinfo.weather.model.Wind;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestPropertySource;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,11 +26,13 @@ import java.util.Map;
 
 import static org.mockito.Mockito.*;
 
+@TestPropertySource(locations="classpath:test.properties") //doesn't work with yaml, might need a workaround
 class WeatherCacheSchedulingServiceTest {
     private final WeatherService weatherServiceMock = mock(WeatherService.class);
-    private final FavoriteLocationService favoriteLocationServiceMock = mock(FavoriteLocationService.class);
 
-    private static WeatherCacheSchedulingService WeatherCacheSchedulingService;
+    public static MockWebServer mockBackEnd;
+
+    private static WeatherCacheSchedulingService weatherCacheSchedulingService;
 
     private final Location location1 = new Location(12.34, 56.67);
     private final Location location2 = new Location(-12.34, -56.78);
@@ -35,8 +44,20 @@ class WeatherCacheSchedulingServiceTest {
     private final LocalDate date = Conversion.convertDate(1643536800).toLocalDate();
     private Map<LocalDate, WeatherConditions> weatherConditionsMap, weatherConditionsMapWithAlerts;
 
+    @BeforeAll
+    static void setUp() throws IOException {
+        mockBackEnd = new MockWebServer();
+        mockBackEnd.start(8086);
+
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        mockBackEnd.shutdown();
+    }
+
     @BeforeEach
-    void setUp() {
+    void setup() {
         //Initializing data variables
         {
             Temperature temperature = new Temperature(1.64, 1.09, -0.16, -0.94);
@@ -68,24 +89,26 @@ class WeatherCacheSchedulingServiceTest {
             favoriteLocation1.setId(1L);
             favoriteLocation2.setId(2L);
         }
-        WeatherCacheSchedulingService = new WeatherCacheSchedulingService(weatherServiceMock, favoriteLocationServiceMock);
+        weatherCacheSchedulingService = new WeatherCacheSchedulingService(weatherServiceMock);
+        weatherCacheSchedulingService.setFavoriteURI("http://localhost:8086/api/v1/weather-info/favorites");
     }
 
     @Test
-    public void When_twoFavoriteLocationsStored_Then_GetWeatherAndEvictWeatherCacheValueIsCalledTwiceEach() {
+    public void When_twoFavoriteLocationsStored_Then_GetWeatherAndEvictWeatherCacheValueIsCalledTwiceEach()
+            throws JsonProcessingException {
         List<FavoriteLocation> favoriteLocations = new ArrayList<>();
         favoriteLocations.add(favoriteLocation1);
         favoriteLocations.add(favoriteLocation2);
-
         when(weatherServiceMock.getWeather(location1))
                 .thenReturn(weatherConditionsMap);
         when(weatherServiceMock.getWeather(location2))
                 .thenReturn(weatherConditionsMapWithAlerts);
 
-        when(favoriteLocationServiceMock.getAllFavorites())
-                .thenReturn(favoriteLocations);
+        mockBackEnd.enqueue(new MockResponse()
+                .setBody(new ObjectMapper().writeValueAsString(favoriteLocations))
+                .addHeader("Content-Type", "application/json"));
 
-        WeatherCacheSchedulingService.cacheWeatherForFavoriteLocations();
+        weatherCacheSchedulingService.cacheWeatherForFavoriteLocations();
 
         verify(weatherServiceMock, times(2)).getWeather(any());
         verify(weatherServiceMock, times(2)).evictWeatherCacheValue(any());
@@ -93,7 +116,7 @@ class WeatherCacheSchedulingServiceTest {
 
     @Test
     public void When_clearAllCacheCalled_Then_evictAllWeatherCacheIsCalled() {
-        WeatherCacheSchedulingService.clearAllCache();
+        weatherCacheSchedulingService.clearAllCache();
 
         verify(weatherServiceMock).evictAllWeatherCache();
     }
